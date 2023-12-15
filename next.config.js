c0ffd57b50742382bffc1346bfdc7b0ae1d36f9e
Johannes Bekele <76441090+JohannesSBA@users.noreply.github.com>
2023-12-15 04:09:54 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" }, // Adjust this according to your project structure
+      // Add other paths as needed
+    };
+  },
+}
 
 module.exports = nextConfig
